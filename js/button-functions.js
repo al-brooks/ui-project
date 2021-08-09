5de0a68cd93b11692a5a55cd6785ab76a5c62dc8
amaso7 <80641831+amaso7@users.noreply.github.com>
2021-08-09 14:58:15 -0400

    Update button-functions.js

--- js/button-functions.js
+++ js/button-functions.js
@@ -1,7 +1,19 @@
 const gifButton = document.getElementById("gifButton")
+const stickerButton = document.getElementById("stickerButton")
+const allButton = document.getElementById("allButton")
 // const gifFrame = document.getElementById("gifFrame")
 
 gifButton.onclick = function () {
+    console.log("test");
+    if (gifFrame.style.display !== "none") {
+        gifFrame.style.display = "none";
+    } else {
+        gifFrame.style.display = "flex";
+    }
+  };
+
+// Sticker button action only view stickers 
+stickerButton.onclick = function () {
     console.log("test");
     if (gifFrame.style.display !== "none") {
         gifFrame.style.display = "none";
