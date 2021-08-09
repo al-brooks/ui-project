6c8b10db258cc20c39d3f435bceb04113c4d23c2
Tim Oh <timoh@Tims-MacBook-Pro.local>
2021-08-09 14:34:17 -0400

    a

--- js/giphy-api.js
+++ js/giphy-api.js
@@ -20,7 +20,7 @@ async function fetchGifData(GIF_URL) {
         let gifData = await response.json()
 
         let gifItems = gifData.data.map((item) => {
-            return `<img src="${item.images.downsized_still.url}" hover_url="${item.images.downsized_medium.url}" unhover_url="${item.images.downsized_still.url}" onmouseover="hover(this)" onmouseout="unhover(this)"/>`
+            return `<img src="${item.images.downsized_still.url}" hover_url="${item.images.downsized_medium.url}" unhover_url="${item.images.downsized_still.url}" onmouseover="hover(this)" onmouseout="unhover(this) onmouseout="unhover(this)"/>`
         })
         gifFrame.innerHTML = gifItems.join("")
     } catch (error) {
@@ -38,6 +38,11 @@ function unhover(image) {
     let unhover_url = image.getAttribute("unhover_url")
     console.log(unhover_url)
     image.setAttribute('src', `${unhover_url}`)
+}
+
+function displayEmbedUrl(image) {
+    let embed_url = image.getAttribute("embed_url")
+    mainContent.innerHTML = `<iframe src="${embed_url}" width="50%" height="50%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>`
 }
 
 // Variables for HTML 
