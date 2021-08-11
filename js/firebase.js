432b0bf4873fb5b79efd0b95c3b0f399532f626d
Alexander Brooks <alexanderlawbrooks@gmail.com>
2021-08-11 11:56:56 -0400

    working on embed url like button

--- js/firebase.js
+++ js/firebase.js
@@ -1,3 +1,33 @@
+// to-do: |
+// remove onclick events after testing and implement form event listeners
+// note:  remember to use prevent default
+
+// code-block:  display functions for user - they're only called if
+// user is signed in (that code is at the bottom of the page)
+
+const gifList = document.querySelector('#gifList');
+
+// function addLikeToDb() {
+//   const form = document.querySelector('#likeBtn');
+//   form.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     dataBase.collection('Gifs').add({ title: title, embed_url: embed_url });
+//   });
+// }
+
+function displayGifs(data) {
+  data.forEach((doc) => {
+    const gif = doc.data();
+    const li = `
+          <li>
+              Embed Url: ${gif.embed_url} | Title: ${gif.title} | Likes: ${gif.likes}
+          </li>
+      `;
+    gifList.insertAdjacentHTML('beforeend', li);
+  });
+}
+
+// code-block:  User Sign In
 function signUp() {
   const email = document.getElementById('email');
   const password = document.getElementById('password');
@@ -20,22 +50,38 @@ function signIn() {
 
   alert(`Signed In ${email.value}`);
 
-  // Take use to another page
-  location.href = '/ui-project/index.html';
+  // Takes user back to the website
+  //   location.href = '/ui-project/index.html';
 }
 
 function signOut() {
   auth.signOut();
   alert('Signed Out');
+  // note:  adding this to clear the login page for now
+  window.location.reload();
 }
 
+// tidbit:  below tracks current users auth status - are they logged in or not
 auth.onAuthStateChanged(function (user) {
   if (user) {
+    //User is signed in - can choose what to show them
     const email = user.email;
     alert(`Active User ${email}`);
-    //is signed in
+
+    // code-block:  Get Data for user account (pull Gifs from collection)
+    // tidbit:  database collection is Async, so it returns a promise
+
+    dataBase
+      .collection('Gifs')
+      .get()
+      .then((querySnapshot) => {
+        displayGifs(querySnapshot.docs);
+      });
   } else {
+    //User is signed out
     alert('No Active User');
-    // no user signed in
+
+    // tidbit:  below code will remove displayed data once user logs out
+    displayGifs([]);
   }
 });
--- js/giphy-api.js
+++ js/giphy-api.js
@@ -1,135 +1,168 @@
 // ASYNC function for fetching searched Stickers
 async function fetchStickerData(STICK_URL) {
-    try {
-        let response = await fetch(STICK_URL)
-        let stickData = await response.json()
+  try {
+    let response = await fetch(STICK_URL);
+    let stickData = await response.json();
 
-        let stickItems = stickData.data.map((item) => {
-            return `<img src="${item.images.downsized_still.url}" hover_url="${item.images.downsized_medium.url}" unhover_url="${item.images.downsized_still.url}" embed_url="${item.embed_url}" onmouseover="hover(this)" onmouseout="unhover(this)" onclick="displayEmbedUrl(this)"/>`
-        })
-        stickerCarousel.innerHTML = stickItems.join("")
-    } catch (error) {
-        console.log(error)
-    }
+    let stickItems = stickData.data.map((item) => {
+      return `<img src="${item.images.downsized_still.url}" hover_url="${item.images.downsized_medium.url}" unhover_url="${item.images.downsized_still.url}" embed_url="${item.embed_url}" onmouseover="hover(this)" onmouseout="unhover(this)" onclick="displayEmbedUrl(this)"/>`;
+    });
+    stickerCarousel.innerHTML = stickItems.join('');
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 // ASYNC function for fetching searched Gifs
 async function fetchGifData(GIF_URL) {
-    try {
-        let response = await fetch(GIF_URL)
-        let gifData = await response.json()
+  try {
+    let response = await fetch(GIF_URL);
+    let gifData = await response.json();
 
-        let gifItems = gifData.data.map((item) => {
-            return `<img src="${item.images.downsized_still.url}" hover_url="${item.images.downsized_medium.url}" unhover_url="${item.images.downsized_still.url}" embed_url="${item.embed_url}"
-            title="${item.title}" onmouseover="hover(this)" onmouseout="unhover(this)" onclick="displayEmbedUrl(this)"/>`
-        })
-        gifFrame.innerHTML = gifItems.join("")
-    } catch (error) {
-        console.log(error)
-    }
+    let gifItems = gifData.data.map((item) => {
+      return `<img src="${item.images.downsized_still.url}" hover_url="${item.images.downsized_medium.url}" unhover_url="${item.images.downsized_still.url}" embed_url="${item.embed_url}"
+            title="${item.title}" onmouseover="hover(this)" onmouseout="unhover(this)" onclick="displayEmbedUrl(this)"/>`;
+    });
+    gifFrame.innerHTML = gifItems.join('');
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function resetAttributes() {
-    gifFrame.removeAttribute("style")
-    stickerCarousel.removeAttribute("style")
-    mobileCategoryBtns.removeAttribute("style")
-    embedUrlDisplay.style.display = "none"
+  gifFrame.removeAttribute('style');
+  stickerCarousel.removeAttribute('style');
+  mobileCategoryBtns.removeAttribute('style');
+  embedUrlDisplay.style.display = 'none';
 }
 
 function hover(image) {
-    let hover_url = image.getAttribute("hover_url")
-    image.setAttribute('src', `${hover_url}`)
+  let hover_url = image.getAttribute('hover_url');
+  image.setAttribute('src', `${hover_url}`);
 }
 
 function unhover(image) {
-    let unhover_url = image.getAttribute("unhover_url")
-    image.setAttribute('src', `${unhover_url}`)
+  let unhover_url = image.getAttribute('unhover_url');
+  image.setAttribute('src', `${unhover_url}`);
 }
 
+// function addLikeToDb() {
+//   const form = document.querySelector('#likeBtn');
+//   form.addEventListener('click', (event) => {
+//     event.preventDefault();
+//     dataBase.collection('Gifs').add({ title: title, embed_url: embed_url });
+//   });
+// }
+
 function displayEmbedUrl(image) {
-    if (embedUrlDisplay.style.display === "none") {
-        embedUrlDisplay.removeAttribute("style")
-    }
-    let embed_url = image.getAttribute("embed_url")
-    let title = image.getAttribute("title")
-    embedUrlDisplay.innerHTML = `<div id="parentDiv">
+  if (embedUrlDisplay.style.display === 'none') {
+    embedUrlDisplay.removeAttribute('style');
+  }
+  let embed_url = image.getAttribute('embed_url');
+  let title = image.getAttribute('title');
+  embedUrlDisplay.innerHTML = `
+    <div id="parentDiv">
         <header id="header">
             <button id="backButton" onClick="window.location.reload();">Back</button>
         </header>
         
         <div id="iframeContainer">
         <h2>${title}</h2>
+        <form id="likeBtn">
+            <input type="text" name="${title}" />
+            <input type="text" name="${embed_url}" />
+            <button>Like</button>
+        </form>
+        <div id="likeCounter">1</div>
             <iframe src="${embed_url}" width="50%" height="50%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
         </div>
     </div>`;
-    mobileCategoryBtns.style.display = "none"
-    stickerCarousel.style.display = "none"
-    gifFrame.style.display = "none"
+
+  const form = document.querySelector('#likeBtn');
+  form.addEventListener('click', (event) => {
+    likes++;
+    event.preventDefault();
+    dataBase
+      .collection('Gifs')
+      .doc(title)
+      .set({ title: title, embed_url: embed_url, likes: likes })
+      .then(() => {
+        console.log(`Document was successfully written with ID: ${title}`);
+        gifRefs.push(title);
+      });
+  });
+
+  // to-do:  onclick function for like button: onClick="addLikeToDb();"
+  mobileCategoryBtns.style.display = 'none';
+  stickerCarousel.style.display = 'none';
+  gifFrame.style.display = 'none';
+
+  return likes;
 }
 
+// database gif ref #s - passes :
+const gifRefs = [];
+let likes = 0;
 
-// Variables for HTML 
-const textboxButtonForm = document.getElementById("textButtonForm")
-const embedUrlDisplay = document.getElementById("embedUrlDisplay")
-const wordTextBox = document.getElementById("wordTextBox")
-const gifFrame = document.getElementById("gifFrame")
-const stickerCarousel = document.getElementById("stickerCarousel")
-const mobileCategoryBtns = document.getElementById("mobileCategoryBtns")
-const gifButton = document.getElementById("gifButton")
-const stickerButton = document.getElementById("stickerButton")
-const allButton = document.getElementById("allButton")
-const sideBtns = document.getElementsByClassName("sideBtns")
+// Variables for HTML
+const textboxButtonForm = document.getElementById('textButtonForm');
+const embedUrlDisplay = document.getElementById('embedUrlDisplay');
+const wordTextBox = document.getElementById('wordTextBox');
+const gifFrame = document.getElementById('gifFrame');
+const stickerCarousel = document.getElementById('stickerCarousel');
+const mobileCategoryBtns = document.getElementById('mobileCategoryBtns');
+const gifButton = document.getElementById('gifButton');
+const stickerButton = document.getElementById('stickerButton');
+const allButton = document.getElementById('allButton');
+const sideBtns = document.getElementsByClassName('sideBtns');
 
-const limitDisplay = 10
+const limitDisplay = 10;
 
-const STICK_INIT_URL = `https://api.giphy.com/v1/stickers/search?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&q=$welcome&limit=${limitDisplay}&offset=0&rating=g&lang=en`
-const GIF_INIT_URL = `https://api.giphy.com/v1/gifs/search?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&q=$welcome&limit=${limitDisplay}&offset=0&rating=g&lang=en`
-
+const STICK_INIT_URL = `https://api.giphy.com/v1/stickers/search?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&q=$welcome&limit=${limitDisplay}&offset=0&rating=g&lang=en`;
+const GIF_INIT_URL = `https://api.giphy.com/v1/gifs/search?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&q=$welcome&limit=${limitDisplay}&offset=0&rating=g&lang=en`;
 
 // Initial page load: Calling ASYNC functions to fetch random stickers/gifs for the landing page
-fetchStickerData(STICK_INIT_URL)
-fetchGifData(GIF_INIT_URL)
-
+fetchStickerData(STICK_INIT_URL);
+fetchGifData(GIF_INIT_URL);
 
 // Sidebar button calls
 for (let i = 0; i < sideBtns.length; i++) {
-    if (sideBtns[i].innerText === 'Trending') {
-        sideBtns[i].addEventListener('click', function() {
-            const STICK_TREND_URL = `https://api.giphy.com/v1/stickers/trending?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&limit=${limitDisplay}&rating=g`
+  if (sideBtns[i].innerText === 'Trending') {
+    sideBtns[i].addEventListener('click', function () {
+      const STICK_TREND_URL = `https://api.giphy.com/v1/stickers/trending?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&limit=${limitDisplay}&rating=g`;
 
-            const GIF_TREND_URL = `https://api.giphy.com/v1/gifs/trending?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&limit=${limitDisplay}&rating=g`
+      const GIF_TREND_URL = `https://api.giphy.com/v1/gifs/trending?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&limit=${limitDisplay}&rating=g`;
 
-            resetAttributes()
-            fetchStickerData(STICK_TREND_URL)
-            fetchGifData(GIF_TREND_URL)
-        })
-    } else {
-        sideBtns[i].addEventListener('click', function() {
-            let keyword = sideBtns[i].innerText;
+      resetAttributes();
+      fetchStickerData(STICK_TREND_URL);
+      fetchGifData(GIF_TREND_URL);
+    });
+  } else {
+    sideBtns[i].addEventListener('click', function () {
+      let keyword = sideBtns[i].innerText;
 
-            let stick_url = `https://api.giphy.com/v1/stickers/search?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&q=$${keyword}&limit=${limitDisplay}&offset=0&rating=g&lang=en`
+      let stick_url = `https://api.giphy.com/v1/stickers/search?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&q=$${keyword}&limit=${limitDisplay}&offset=0&rating=g&lang=en`;
 
-            let gif_url = `https://api.giphy.com/v1/gifs/search?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&q=$${keyword}&limit=${limitDisplay}&offset=0&rating=g&lang=en`
+      let gif_url = `https://api.giphy.com/v1/gifs/search?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&q=$${keyword}&limit=${limitDisplay}&offset=0&rating=g&lang=en`;
 
-            resetAttributes()
-            fetchStickerData(stick_url)
-            fetchGifData(gif_url)
-        })
-    }
+      resetAttributes();
+      fetchStickerData(stick_url);
+      fetchGifData(gif_url);
+    });
+  }
 }
 
-
 // When the user click the search button    - Eventlistener for <form> tag for validation
-textboxButtonForm.addEventListener("submit", function(e) {
-    e.preventDefault()
+textboxButtonForm.addEventListener('submit', function (e) {
+  e.preventDefault();
 
-    const keyword = wordTextBox.value
+  const keyword = wordTextBox.value;
 
-    const STICK_SEARCH_URL = `https://api.giphy.com/v1/stickers/search?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&q=${keyword}&limit=${limitDisplay}&offset=0&rating=g&lang=en`
-    const GIF_SEARCH_URL = `https://api.giphy.com/v1/gifs/search?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&q=${keyword}&limit=${limitDisplay}&offset=0&rating=g&lang=en`
+  const STICK_SEARCH_URL = `https://api.giphy.com/v1/stickers/search?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&q=${keyword}&limit=${limitDisplay}&offset=0&rating=g&lang=en`;
+  const GIF_SEARCH_URL = `https://api.giphy.com/v1/gifs/search?api_key=ETsIe95S9ra8O2xYkHRPGcwr1X49fBN4&q=${keyword}&limit=${limitDisplay}&offset=0&rating=g&lang=en`;
 
-    // Calling ASYNC functions
-    resetAttributes()
-    fetchStickerData(STICK_SEARCH_URL)
-    fetchGifData(GIF_SEARCH_URL)
-})
+  // Calling ASYNC functions
+  resetAttributes();
+  fetchStickerData(STICK_SEARCH_URL);
+  fetchGifData(GIF_SEARCH_URL);
+});
